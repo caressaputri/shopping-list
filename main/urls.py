1e5b59255fede143301b6a0e69cc115fed14e501
caressaputri <caressaputriyuliantoro@gmail.com>
2023-09-27 14:24:22 +0700

    Tutorial 4 Selesai

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
 from main.views import logout_user
+from main.views import edit_product
 
 app_name = 'main'
 
@@ -21,6 +22,7 @@ urlpatterns = [
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
     path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product')
 
 ]
--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 import environ # Tambahkan kode berikut
 import os # Tambahkan kode berikut
 
+
+
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
